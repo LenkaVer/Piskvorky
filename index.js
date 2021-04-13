6f09a29e8e3d002a10498949587dfab8ac327b9e
Lenka Verzichová <lenkaverzichova@gmail.com>
2021-04-13 14:51:47 +0200

    Hotová hra

--- index.js
+++ index.js
@@ -5,27 +5,26 @@ let naTahu = 'circle';
 const tlacitka = document.querySelectorAll('button');
 
 const vypisTlacitko = (tlacitko) => {
-  tlacitko.addEventListener('click', (event) => {
-    const stiskleTlacitko = event.target;
+  tlacitko.addEventListener('click', () => {
     const ikonaNaTahu = document.querySelector('.ikona-na-tahu');
-    // if (
-    //   stiskleTlacitko.classList.contains('tlacitko--kolecko') ||
-    //   stiskleTlacitko.classList.contains('tlacitko--krizek')
-    // ) {
-    //   return;
-    // } tahle část kódu mi nefunguje správně
+    if (
+      tlacitko.classList.contains('tlacitko--kolecko') ||
+      tlacitko.classList.contains('tlacitko--krizek')
+    ) {
+      return;
+    }
     if (naTahu === 'circle') {
-      stiskleTlacitko.classList.add('tlacitko--kolecko');
+      tlacitko.classList.add('tlacitko--kolecko');
       naTahu = 'cross';
       ikonaNaTahu.src = 'img/cross.svg';
       ikonaNaTahu.alt = 'na tahu krizek';
     } else {
-      stiskleTlacitko.classList.add('tlacitko--krizek');
+      tlacitko.classList.add('tlacitko--krizek');
       naTahu = 'circle';
       ikonaNaTahu.src = 'img/circle.svg';
       ikonaNaTahu.alt = 'na tahu kolecko';
     }
-    event.target.setAttribute('disabled', '');
+    tlacitko.setAttribute('disabled', '');
   });
 };
 tlacitka.forEach(vypisTlacitko);
